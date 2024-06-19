bff523e8b07cfce6a2e49dbc872935db9b798f36
chukumela <chukumelawilson@gmail.com>
2024-06-19 08:59:55 +0100

    chores(Post Form) Remove unused prop.

--- src/app/(admin)/admin/posts/new/page.tsx
+++ src/app/(admin)/admin/posts/new/page.tsx
@@ -1,12 +1,8 @@
 import Wrapper from "@/components/shared/Wrapper";
 import PostForm from "../../_components/PostForm";
 import PageHeading from "@/components/shared/PageHeading";
-import { findUserById } from "@/lib/actions/user.actions";
-import { auth } from "@/auth";
 
 export default async function NewPostPage() {
-  const session = await auth();
-  const user = await findUserById(session?.user.id as string);
   
   return (
     <section className="my-20">
@@ -16,7 +12,7 @@ export default async function NewPostPage() {
           pageSubtitle="Create A New Post"
         />
         <div className="w-full flex justify-center my-20">
-          <PostForm type="Create" sessionUser={user.username} />
+          <PostForm type="Create" />
         </div>
       </Wrapper>
     </section>
